f000029ab20e95b2d0f39430fdbfadb1132fb60c
StevenMilano <36162914+StevenMilano@users.noreply.github.com>
2020-09-16 09:57:30 -0400

    Added Delete and Add random card functionality
    Drill to learn Callback Props

--- App.test.js
+++ App.test.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
+import Card from './Card.js';
+import List from './List.js';
+import renderer from 'react-test-renderer'
+import STORE from './store.js'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Trelloyes/);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('Card component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Card name="Card"/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+});
+
+describe('List component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List
+      key={'1'}
+      header={'First list'}
+      cards={[{ id: 'a', title: 'First card', content: 'lorem ipsum' }]}
+    />, div);
+  ReactDOM.unmountComponentAtNode(div);
+},
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List 
+              key={'1'}
+              header={'First list'}
+              cards={[{ id: 'a', title: 'First card', content: 'lorem ipsum' }]}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  }));
 });
